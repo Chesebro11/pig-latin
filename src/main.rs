67433bb28fea76c7749e9431f68272ef8e5c07d2
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-17 10:00:14 -0800

    first commit notes outline

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Convert string to pig latin, maybe I should do this based off of user input?
+// user inputs a string and that gets translated to pig latin
+// What kind of issues could come from this?
+// Would have to either limit to one word at a time somehow, or write a function
+// that splits each word apart then translates each to pig latin and puts it back together>
+// idk man I hate strings
+// words that start with a vowel will have hay added instead of first letter + "ay"
+// re reference the utf-8 encoding section.
+
+fn main() {
+    println!("Hello, world!");
+}
